834a7f3e0fdc24ea0df67406d668c6d7a718a2c3
Ishan Jindal <ishanjindal2003@gmail.com>
2023-02-13 17:42:57 +0530

    sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+    else if ( typeof(start) != "number" || typeof(end) != "number"){
+        return "ERROR";
+    }
+    else if(start<end){
+    while(start<=end){
+        sum = sum+start;
+        start++;
+    }
+    return sum;
+}
+    else if (start>end) {
+        while(start>=end){
+            sum = sum+start;
+            start--;
+        }
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
