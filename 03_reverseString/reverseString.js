09db51f1b3dd276a5ef861c457b9c28182fbca2d
Ishan Jindal <ishanjindal2003@gmail.com>
2023-02-13 15:04:08 +0530

    reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let revWord = [];
+    for (let index = word.length-1; index >= 0; index--) {
+        revWord.push(word[index]);
+        
+    }
 
+    return revWord.join("")
 };
 
 // Do not edit below this line
