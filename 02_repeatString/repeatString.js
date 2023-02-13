05180f882070055ae03226e27fd589f8049e926f
Ishan Jindal <ishanjindal2003@gmail.com>
2023-02-13 14:40:37 +0530

    repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeat) {
+    let answer = "";
+    if (repeat<0){
+        return "ERROR";
+    }
+    for (let index = 0; index < repeat; index++) {
+        answer = answer+word;   
+    }
+    return answer;
 };
 
 // Do not edit below this line
